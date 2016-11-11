506a93895c1d1af62e71492ff20fe15694baa333
Thomas Torsney-Weir <torsneyt@gmail.com>
2016-11-11 10:22:23 +0100

    move config stuff into packages and group all subpackages

---
+++ nix/packages.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+
+let
+  myvim = import ./vim.nix;
+  gdrive = import ./gdrive.nix;
+  zshrc = import ./zsh-config.nix;
+  bower = nodePackages.bower;
+  ps = haskellPackages.purescript;
+
+in
+  { inherit
+      powerline-fonts
+      anonymousPro
+
+      cabal-install
+      ghc
+      stack
+      nodejs
+      ps
+
+      gnumake
+
+      git
+      mutt
+      rxvt_unicode-with-plugins
+      myvim
+      zshrc
+      gdrive
+      ;
+  }
