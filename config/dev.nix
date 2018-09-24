e0203c327ac54b9955a8bb50b7c930d03440adf9
Thomas Torsney-Weir <torsneyt@gmail.com>
2018-09-24 16:37:10 +0200

    binary cache for reflex

--- config/dev.nix
+++ config/dev.nix
@@ -18,4 +18,7 @@
     # general
     gnumake
   ];
+
+  nix.binaryCaches = [ "https://cache.nixos.org/" "https://nixcache.reflex-frp.org" ];
+  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
 }
