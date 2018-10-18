2b1b3756ac0c9977b04c5fb90f078544dfc9cf85
Thomas Torsney-Weir <torsneyt@gmail.com>
2018-10-18 15:40:26 +0200

    bump home-manager version

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -160,5 +160,5 @@
   };
 
   programs.home-manager.enable = true;
-  programs.home-manager.path = https://github.com/rycee/home-manager/archive/release-18.03.tar.gz;
+  programs.home-manager.path = https://github.com/rycee/home-manager/archive/release-18.09.tar.gz;
 }
