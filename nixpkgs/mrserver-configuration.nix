b262dc0608e87c3f46bb3de7e216eab6cc803895
Thomas Torsney-Weir <torsneyt@gmail.com>
2018-12-05 09:10:31 +0000

    update for new dir layout

--- nixpkgs/mrserver-configuration.nix
+++ nixpkgs/mrserver-configuration.nix
@@ -2,8 +2,8 @@
 
 {
   imports = [
-    ../hardware-configuration.nix
-    ../networking.nix # generated at runtime by nixos-infect
+    ../../hardware-configuration.nix
+    ../../networking.nix # generated at runtime by nixos-infect
     ./config/base.nix
   ];
 
