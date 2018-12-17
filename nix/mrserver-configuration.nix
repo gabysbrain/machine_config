89821d4101c9eda6f592d76245f7e94161da5cbc
Thomas Torsney-Weir <torsneyt@gmail.com>
2018-12-17 07:59:38 +0000

    update file nodes

--- nix/mrserver-configuration.nix
+++ nix/mrserver-configuration.nix
@@ -8,6 +8,12 @@
   ];
 
   boot.cleanTmpDir = true;
+  boot.kernel.sysctl = { # need more watch nodes
+    # Note that inotify watches consume 1kB on 64-bit machines.
+    "fs.inotify.max_user_watches"   = 1048576;   # default:  8192
+    "fs.inotify.max_user_instances" =    1024;   # default:   128
+    "fs.inotify.max_queued_events"  =   32768;   # default: 16384
+  };
   networking.hostName = "mrserver";
   networking.firewall = {
     allowPing = true;
