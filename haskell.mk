fce0cc45d460fb3370413c5d8dc3b8f5b9046cb3
Thomas Torsney-Weir <torsneyt@gmail.com>
2018-12-17 15:43:10 +0100

    haskell makefile not needed anymore

--- haskell.mk
+++
@@ -1,8 +0,0 @@
-
-# Makefile for all the haskell stuff
-
-# TODO: fix this so that hlint and ghc-mod are auto-detected somehow
-~/.stack:
-	(stack setup || rm -rf $<)
-	(stack install hlint ghc-mod || rm -rf $<)
-
