2ff98e232a352a4b9066d3196e3dc15dcd675121
Thomas Torsney-Weir <torsneyt@gmail.com>
2018-12-22 17:07:44 +0100

    new improved workspaces

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -19,11 +19,24 @@ import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed
 
+import XMonad.Util.NamedActions
 import XMonad.Util.Run(spawnPipe)
-import System.IO(hPutStrLn)
+
+import System.IO
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
+
+-- Workspaces
+wsGen = "Gen"
+wsWk1 = "Wk1"
+wsWk2 = "Wk2"
+wsWk3 = "Wk3"
+wsCom = "Com"
+wsDoc = "Doc"
+wsAV  = "AV"
+wsTmp = "Tmp"
+wsSys = "Sys"
 
 ---
 --- derived from https://gist.github.com/Tzbob/7362371
@@ -37,7 +50,7 @@ myFocusFollowsMouse = True
 
 myModMask = mod4Mask
 
-myWorkspaces = ["1 sh","2 ed","3 www","4 mail", "5 doc", "6 ."]
+myWorkspaces = [wsGen,wsWk1,wsWk2,wsWk3, wsCom, wsDoc, wsAV, wsTmp, wsSys]
 myMainColor = "#333333"
 myBgColor = "#FEFEFE"
 myTextColor = "#282828"
@@ -162,18 +175,18 @@ myLayout = smartBorders $ avoidStruts $ tiledL ||| bspL ||| tabbedL
 -- Window rules:
 -- > xprop | grep WM_CLASS
 -------------------------------------------------------------------------------
-myManageHook = manageDocks <+> composeAll
-    [ isFullscreen --> doFullFloat
-    -- special desktops
-    , className =? "Chromium-browser" --> doShift "3 www"
-    , className =? "Firefox" --> doShift "3 www"
-    , className =? "Gvim" --> doShift "2 ed"
-    , className =? "okular" --> doShift "5 doc"
+myManageHook = manageDocks <+> composeOne
+    [ isFullscreen -?> doFullFloat
+    , isBrowserDialog -?> forceCenterFloat
+    , isRole =? "pop-up" -?> forceCenterFloat
 
     -- floating windows
-    , className =? "Pinentry" --> doFloat
-    , className =? "Qpdfview" --> doFloat
+    , className =? "Pinentry" -?> doFloat
+    , className =? "Qpdfview" -?> doFloat
     ]
+  where
+  isBrowserDialog = isDialog <&&> (className =? "Chromium-browser" <||> className =? "Firefox")
+  isRole = stringProperty "WM_WINDOW_ROLE"
 
 -- Event handling
 -------------------------------------------------------------------------------
@@ -196,6 +209,17 @@ myPP statusPipe = xmobarPP {
 }
 
 myLogHook pipe = dynamicLogWithPP (myPP pipe)  -- >> updatePointer (Relative 0.5 0.5)
+
+
+-- Display keyboard mappings using zenity
+-- from https://github.com/thomasf/dotfiles-thomasf-xmonad/
+--              blob/master/.xmonad/lib/XMonad/Config/A00001.hs
+showKeybindings :: [((KeyMask, KeySym), NamedAction)] -> NamedAction
+showKeybindings x = addName "Show Keybindings" $ io $ do
+    h <- spawnPipe "zenity --text-info --font=terminus"
+    hPutStr h (unlines $ showKm x)
+    hClose h
+    return ()
 
 -- Startup hook
 -------------------------------------------------------------------------------
@@ -227,4 +251,24 @@ myConfig statusPipe = def {
 main = do
   statusPipe <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
   xmonad . ewmh $ myConfig statusPipe
+    -- $ addDescrKeys ((myModMask, xK_F1), showKeybindings) myKeys
+
+-- from:
+-- https://github.com/pjones/xmonadrc/blob/master/src/XMonad/Local/Action.hs
+--
+-- Useful when a floating window requests stupid dimensions.  There
+-- was a bug in Handbrake that would pop up the file dialog with
+-- almost no height due to one of my rotated monitors.
+
+forceCenterFloat :: ManageHook
+forceCenterFloat = doFloatDep move
+  where
+    move :: W.RationalRect -> W.RationalRect
+    move _ = W.RationalRect x y w h
+
+    w, h, x, y :: Rational
+    w = 1/3
+    h = 1/2
+    x = (1-w)/2
+    y = (1-h)/2
 
