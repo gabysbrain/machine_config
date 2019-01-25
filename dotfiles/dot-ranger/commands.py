eb067aa898a50f20f9b30223dd94a04a725b3ce0
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-01-25 09:55:01 +0100

    fix extraction stuff

--- dotfiles/dot-ranger/commands.py
+++ dotfiles/dot-ranger/commands.py
@@ -14,7 +14,7 @@ import os
 
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
-
+from ranger.core.loader import CommandLoader
 
 # Any class that is a subclass of "Command" will be integrated into ranger as a
 # command.  Try typing ":my_edit<ENTER>" in ranger!
