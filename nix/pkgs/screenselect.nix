3af558fa914fe7cb97c5f1655d880b7287995def
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-02-24 13:00:48 +0100

    properly select a mirrored screen

--- nix/pkgs/screenselect.nix
+++ nix/pkgs/screenselect.nix
@@ -18,7 +18,7 @@ pkgs.writeShellScriptBin "screenselect" ''
     chosen=$(printf "docking station\\ndual\\nmain\\nother" | dmenu -i -p "Select display:") &&
     case "$chosen" in
       "docking station") $XRANDR --output DP1-1 --off; $XRANDR --output DP1-1 --mode '1920x1080' --left-of eDP1 ;;
-      dual) $XRANDR --output $MAIN --auto $(echo "$screens" | grep -v $MAIN | awk '{print "--output", $1, "--off"}' | tr '\n' ' ') ;;
+      dual) $XRANDR --output $MAIN --auto $(echo "$screens" | grep -v $MAIN | awk '{print "--output", $1, "--same-as $MAIN"}' | tr '\n' ' ') ;;
       main) $XRANDR --output $MAIN --auto $(echo "$screens" | grep -v $MAIN | awk '{print "--output", $1, "--off"}' | tr '\n' ' ') ;;
       other) $ARANDR ; exit ;;
     esac
