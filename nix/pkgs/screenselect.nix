c0671d2dc7351c35c70ab600fb78a90b3f477a46
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-04-17 08:50:40 +0200

    readjust screen names

--- nix/pkgs/screenselect.nix
+++ nix/pkgs/screenselect.nix
@@ -2,7 +2,8 @@
 with import <nixpkgs> {};
 
 pkgs.writeShellScriptBin "screenselect" ''
-    MAIN="eDP-1"
+    MAIN="eDP1"
+    DOCK="DP1-1"
     XRANDR=${pkgs.xorg.xrandr}/bin/xrandr
     ARANDR=${pkgs.arandr}/bin/arandr
 
@@ -17,7 +18,7 @@ pkgs.writeShellScriptBin "screenselect" ''
     # Let the user choose what screen
     chosen=$(printf "docking station\\ndual\\nmain\\nother" | dmenu -i -p "Select display:") &&
     case "$chosen" in
-      "docking station") $XRANDR --output DP-1-1 --off; $XRANDR --output DP-1-1 --mode '1920x1080' --left-of eDP-1 ;;
+      "docking station") $XRANDR --output $DOCK --off; $XRANDR --output $DOCK --mode '1920x1080' --left-of $MAIN ;;
       dual) $XRANDR --output $MAIN --auto $(echo "$screens" | grep -v $MAIN | awk '{print "--output", $1, "--same-as $MAIN"}' | tr '\n' ' ') ;;
       main) $XRANDR --output $MAIN --auto $(echo "$screens" | grep -v $MAIN | awk '{print "--output", $1, "--off"}' | tr '\n' ' ') ;;
       other) $ARANDR ; exit ;;
