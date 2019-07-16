e0a73e683270f358068ac23a3889b4f7c26d0842
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-07-16 11:09:59 +0100

    new laptop config

---
+++ nix/portege-configuration.nix
@@ -0,0 +1,83 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ../../hardware-configuration.nix
+      ./config/base.nix
+      ./config/dev.nix
+      ./config/writing.nix
+      ./config/laptop.nix
+      ./config/desktop-full.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.grub = {
+    #enable = true;
+    #version = 2;
+    #device = "nodev";
+    #efiSupport = true;
+    #canTouchEfiVariables = true;
+  #};
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/nvme0n1p3";
+      preLVM = true;
+      allowDiscards = true;
+    }
+  ];
+
+  networking.hostName = "philadelphia"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
+
+  # List services that you want to enable:
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # The remaining syncthing config
+  services.syncthing = {
+    enable = true;
+    user = "tom";
+    dataDir = "/home/tom/.config/syncthing";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tom = {
+    home = "/home/tom";
+    description = "Thomas Torsney-Weir";
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    createHome = true;
+    shell = "/run/current-system/sw/bin/zsh";
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+
+}
