1dff0c98b92a163fcd7238040b90e7e2ddc97475
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-11-29 11:43:04 +0000

    switch work email server to gmail

--- nix/home-config/desktop.nix
+++ nix/home-config/desktop.nix
@@ -22,10 +22,11 @@
         userName = "torsneyt@gmail.com";
         address = "torsneyt@gmail.com";
         flavor = "gmail.com";
-        imap = {
-          #host = "imap.gmail.com";
-          tls.enable = true;
-        };
+        # imap through manual offlineimap
+        #imap = {
+          ##host = "imap.gmail.com";
+          #tls.enable = true;
+        #};
         smtp = {
           #host = "smtp.gmail.com";
           tls.enable = true;
@@ -50,10 +51,11 @@
         address = "t.d.torsney-weir@swansea.ac.uk";
         flavor = "plain";
         passwordCommand = "gpg --quiet --for-your-eyes-only --decrypt ~/.password-store/swansea.ac.uk.gpg | head -1";
-        imap = {
-          host = "outlook.office365.com";
-          tls.enable = true;
-        };
+        # imap through gmail/offlineimap
+        #imap = {
+          #host = "outlook.office365.com";
+          #tls.enable = true;
+        #};
         smtp = {
           host = "outlook.office365.com";
           port = 587;
@@ -62,9 +64,10 @@
         };
         folders = {
           inbox = "INBOX";
-          drafts = "Drafts";
-          sent = "Sent Mail";
-          trash = "Deleted Items";
+          drafts = "[Gmail]/Drafts";
+          sent = "[Gmail]/Sent Mail";
+          trash = "[Gmail]/Trash";
+          #archive = "[Gmail]/All Mail";
         };
         msmtp.enable = true;
         notmuch.enable = true;
@@ -78,10 +81,11 @@
         address = "thomas.torsney-weir@univie.ac.at";
         flavor = "plain";
         passwordCommand = "gpg --quiet --for-your-eyes-only --decrypt ~/.password-store/univie.ac.at.gpg | head -1";
-        imap = {
-          host = "imap.univie.ac.at";
-          tls.enable = true;
-        };
+        # imap through manual offlineimap
+        #imap = {
+          #host = "imap.univie.ac.at";
+          #tls.enable = true;
+        #};
         smtp = {
           host = "mail.univie.ac.at";
           tls.enable = true;
