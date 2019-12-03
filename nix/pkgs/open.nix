b64e39d6579de0201ad591f73fd3bc8af448a960
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-12-03 10:07:46 +0000

    hard-code file command path

--- nix/pkgs/open.nix
+++ nix/pkgs/open.nix
@@ -4,7 +4,7 @@
 pkgs.writeShellScriptBin "open" ''
   fname=$1
   OPENER=${pkgs.mimeo}/bin/mimeo
-  case $(file --mime-type $fname -b) in
+  case $(${pkgs.file}/bin/file --mime-type $fname -b) in
     text/*) $EDITOR $fname;;
     image/*) ${pkgs.sxiv}/bin/sxiv -a $fname ;;
     application/pdf) ${pkgs.zathura}/bin/zathura $fname;;
