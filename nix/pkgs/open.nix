8043ef3f2b8c986959bf2d542d3eff2cc942d723
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-12-31 12:27:56 +0000

    properly open filenames with spaces

--- nix/pkgs/open.nix
+++ nix/pkgs/open.nix
@@ -4,11 +4,11 @@
 pkgs.writeShellScriptBin "open" ''
   fname=$1
   OPENER=${pkgs.mimeo}/bin/mimeo
-  case $(${pkgs.file}/bin/file --mime-type $fname -b) in
-    text/*) $EDITOR $fname;;
-    image/*) ${pkgs.sxiv}/bin/sxiv -a $fname ;;
-    application/pdf) ${pkgs.zathura}/bin/zathura $fname;;
-    *) $OPENER $fname ;;
+  case $(${pkgs.file}/bin/file --mime-type "$fname" -b) in
+    text/*) $EDITOR "$fname";;
+    image/*) ${pkgs.sxiv}/bin/sxiv -a "$fname" ;;
+    application/pdf) ${pkgs.zathura}/bin/zathura "$fname";;
+    *) $OPENER "$fname" ;;
   esac
 ''
 
