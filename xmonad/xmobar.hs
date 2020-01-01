5bdc0651fef0896b10ac9fa7816ecc638e456e3d
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-01-01 11:02:20 +0000

    show charging status and cpu freq

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -7,8 +7,9 @@ Config
   , commands = [
       Run Com "/home/tom/.xmonad/xmobar-syncthing-status.sh" [] "st" 30
     , Run Com "/home/tom/.xmonad/net.sh" [] "netstat" 30
-    , Run Battery ["-t", "<left>"] 100
+    , Run Battery ["-t", "<left><acstatus>", "--", "-O", "+", "-i", "", "-o", " <timeleft>", "-L", "10", "-l", "red"] 60
     , Run MultiCpu ["-t", "<total>"] 30
+    , Run CpuFreq ["-t", "<cpu0>GHz"] 30
     , Run Date "%_d %#B %Y <fc=#333333>|</fc> %H:%M" "date" 600
     , Run Alsa "default" "Master" ["-t", "<volume>% <status>"]
     , Run Kbd []
@@ -16,6 +17,6 @@ Config
     ]
   , sepChar = "%"
   , alignSep = "}{"
-  , template = " %StdinReader% }{<fc=#333333>net</fc> <action=`connman-gtk`>%netstat%</action> <fc=#333333>kb</fc> %kbd% <fc=#333333>st</fc> %st% <fc=#333333>cpu</fc> %multicpu% <fc=#333333>vol</fc> %alsa:default:Master% <fc=#333333>bat</fc> %battery% <fc=#333333>|</fc> %date% "
+  , template = " %StdinReader% }{<fc=#333333>net</fc> <action=`connman-gtk`>%netstat%</action> <fc=#333333>kb</fc> %kbd% <fc=#333333>st</fc> %st% <fc=#333333>cpu</fc> %multicpu% (%cpufreq%) <fc=#333333>vol</fc> %alsa:default:Master% <fc=#333333>bat</fc> %battery% <fc=#333333>|</fc> %date% "
   }
 
