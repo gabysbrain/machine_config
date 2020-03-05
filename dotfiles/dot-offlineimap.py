5f19a3eba4085f5852b886941b12e4e6ea644b70
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-03-05 09:21:22 +0000

    remove offlineimap config

--- dotfiles/dot-offlineimap.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2
-
-from subprocess import check_output
-
-def get_pass(name):
-  return check_output("gpg -dq ~/.password-store/" + name + ".gpg", shell=True).splitlines()[0]
-
