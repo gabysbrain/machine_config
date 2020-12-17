c09f63b55f11348cf34b44ccb532315b90e68f62
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-12-17 15:36:05 +0000

    build script also copies to sd card now

--- nix/rpi-image/build_rpi.sh
+++ nix/rpi-image/build_rpi.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-nixos-generate -f sd-aarch64 --system aarch64-linux -c image.nix
+SD=/dev/mmcblk0
+
+BUILDLOG=`nixos-generate -f sd-aarch64 --system aarch64-linux -c image.nix`
+IMGDIR=$(dirname $(echo $BUILDLOG | tail -1))/..
+
+SDIMG=$(ls $IMGDIR/sd-image/*.img)
+
+echo $SDIMG
+
+echo "copying image to ${SD}"
+read -p "Press [Enter] to continue ..."
+
+sudo umount ${SD}p*
+sudo cp --verbose ${SDIMG} ${SD}
