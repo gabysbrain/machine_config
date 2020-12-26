d6f38bee78531b91d9d15fefac50703012052313
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-12-26 11:57:16 +0000

    reorganize rpi config

--- nix/rpi-configuration.nix
+++ nix/rpi-configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, options, ... }:
 {
   imports = [
-    ./config/base.nix
+    #<home-manager/nixos>
     ./wireless.nix
   ];
 
@@ -10,13 +10,20 @@
   boot.loader.raspberryPi.enable = true;
   boot.loader.raspberryPi.version = 3;
   boot.loader.raspberryPi.uboot.enable = true;
+  boot.loader.raspberryPi.firmwareConfig = ''
+     gpu_mem=0
+  '';
+
+  boot.consoleLogLevel = 7;
 
   # Kernel configuration
+  #boot.kernelPackages = pkgs.linuxPackages_5_4;
   boot.kernelPackages = pkgs.linuxPackages_4_19;
-  boot.kernelParams = ["cma=32M"];
+  boot.kernelParams = ["cma=256M"];
 
   # Enable additional firmware (such as Wi-Fi drivers).
   hardware.enableRedistributableFirmware = true;
+  #hardware.enableAllFirmware = true;
 
   # Filesystems
   fileSystems = {
@@ -25,7 +32,8 @@
       fsType = "ext4";
     };
   };
-  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+
+  swapDevices = [ { device = "/swapfile"; size = 1024; } ]; # setting to 2048 broke things...
 
   # Networking (see official manual or `/config/sd-image.nix` in this repo for other options)
   #networking.hostName = "nixpi"; # unleash your creativity!
@@ -39,50 +47,31 @@
   # Packages
   environment.systemPackages = with pkgs; [
     # customize as needed!
-    zsh vim git htop
+    bash zsh vim git
+    raspberrypi-tools
   ];
-
-  # Users
-  users.users.tom = {
-    home = "/home/tom";
-    description = "Thomas Torsney-Weir";
-    extraGroups = [ "wheel" "lp" "lpadmin" "media" ]; # Enable ‘sudo’ for the user.
-    createHome = true;
-    shell = "/run/current-system/sw/bin/zsh";
-    openssh.authorizedKeys.keyFiles = [
-      #"ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAwDPUjo80GFY2FO9bDH9cAXo7n7SiUKjXIHzfRMfsAqD9Rk/puV+W4QRvT0XOZSEZQf3gifcPM/raA35BVmAzAa2jYISWeUWIqYf+AcipFrMKKqS639Q9/GgJL2STr6Gh0EVHsGcFJpuJ8GO5eqnKR0ZYl3j9bpMO/WpgkAw7hUU= tom@katana"
-      /home/tom/.ssh/id_rsa.pub
-    ];
-  };
-  home-manager.users.tom = import ./home-config/server.nix; # needs to be a function
 
   # Miscellaneous
   time.timeZone = "Europe/London"; 
-  #services.openssh.enable = true;
-  #services.openssh.hostKeys = options.services.openssh.hostKeys.default;
 
   # server doesn't compile on raspberry pi
   services.localtime.enable = pkgs.lib.mkForce false;
-
-  services.openssh = {
-    enable = true;
-    permitRootLogin = "prohibit-password";
-    passwordAuthentication = false;
-    challengeResponseAuthentication = false;
-    extraConfig = "Compression no";
-  };
-  
-  users.users.root = {
-    openssh.authorizedKeys.keyFiles = [ /home/tom/.ssh/id_rsa.pub ];
-  };
 
   # WARNING: if you remove this, then you need to assign a password to your user, otherwise
   # `sudo` won't work. You can do that either by using `passwd` after the first rebuild or
   # by setting an hashed password in the `users.users.yourName` block as `initialHashedPassword`.
   security.sudo.wheelNeedsPassword = false;
+  users.users.root = {
+    openssh.authorizedKeys.keyFiles = [ /home/tom/.ssh/id_rsa.pub ];
+  };
+  
 
   # Nix
   boot.cleanTmpDir = true;
+
+  # don't install man pages to save space
+  documentation.man.enable = false;
+  documentation.nixos.enable = false;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "20.09";
--- nix/rpi-image/image.nix
+++ nix/rpi-image/image.nix
@@ -5,6 +5,23 @@
     ../rpi-configuration.nix
   ];
 
+  networking.hostName = "newrpi";
+
+  # auto login as root
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+  };
+  
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ];
+    initialPassword = "nixos";
+  };
+  services.mingetty.autologinUser = "nixos";
+
+  users.users.root.initialPassword = "nixos"; # Log in without a password
+
   # bzip2 compression takes loads of time with emulation, skip it.
   sdImage.compressImage = false;
 }
