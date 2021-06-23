fe46c7b10c29f43be0b9139c6c903172d051f345
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-06-23 10:37:33 +0100

    bracey and lightline are now in nix

--- nix/pkgs/neovim/default.nix
+++ nix/pkgs/neovim/default.nix
@@ -1,32 +1,6 @@
 {pkgs,...}:
 
 let
-  bracey-vim = (pkgs.callPackage ./bracey-vim {}).package;
-  customPlugins.bracey-vim = pkgs.vimUtils.buildVimPlugin {
-    name = "bracey-vim";
-    src = pkgs.fetchFromGitHub {
-      owner = "turbio";
-      repo = "bracey.vim";
-      rev = "912997c5d0dbf42feaba5a4ee5527159a41ff82f";
-      sha256 = "1swsgid5xhgcvw4rqmalcjmkhr7bv00ikfj7b7xsyw0ps9mmcjbw";
-    };
-    #buildInputs = [
-      #nodePkgs.package
-    #];
-    # FIXME: this is a hack because bracey wants node_modules in its server directory
-    postInstall = ''
-      ln -s ${bracey-vim}/lib/node_modules/bracey/node_modules $out/node_modules
-    '';
-  };
-  customPlugins.lightline-bufferline = pkgs.vimUtils.buildVimPlugin {
-    name = "lightline-bufferline";
-    src = pkgs.fetchFromGitHub {
-      owner = "mengelbrecht";
-      repo = "lightline-bufferline";
-      rev = "17683bc5802de7f295f2583a15461e2bc662f98b";
-      sha256 = "1rlamxwk2gm9pyxl9vym9w6rhgimzqa2hjghy3qdqwvif6w8ir6l";
-    };
-  };
   customPlugins.vim-criticmarkup = pkgs.vimUtils.buildVimPlugin {
     name = "vim-criticmarkup";
     src = pkgs.fetchFromGitHub {
@@ -63,14 +37,14 @@ in
       pkgs.vimPlugins.ctrlp
       pkgs.vimPlugins.vim-addon-nix
       pkgs.vimPlugins.lightline-vim
-      customPlugins.lightline-bufferline
+      pkgs.vimPlugins.lightline-bufferline
       pkgs.vimPlugins.vim-obsession
       pkgs.vimPlugins.vimwiki
       pkgs.vimPlugins.vimproc
       pkgs.vimPlugins.vim-easy-align
       pkgs.vimPlugins.vim-slime
       pkgs.vimPlugins.vimtex
-      customPlugins.bracey-vim
+      pkgs.vimPlugins.bracey-vim
       pkgs.vimPlugins.vim-hindent
     ];
   };
