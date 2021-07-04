9c97d25b32fde63a5a8f8a2f7835a8e29774b604
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-07-04 11:24:54 +0200

    start vrvis config

---
+++ nix/config/vrvis.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+let 
+  cacert = pkgs.fetchurl {
+    url = http://ca.vrvis.at/vrvis_ca.pem;
+    sha256 = "0pjni0hx7p6zb71lv5l67cfbg26nbyjd0fc24mn5w4gal7cjyl8s";
+  };
+in 
+{
+  # ca certs
+  security.pki.certificates = [
+    "${builtins.readFile cacert}"
+    #(builtins.readFile ../secrets/vrvis_ca.pem)
+  ];
+}
