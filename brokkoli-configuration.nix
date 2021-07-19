79dcb293eec3f09724639e99e0427a3c73c790b3
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-07-19 10:00:04 +0200

    home-manager config for brokkoli

---
+++ brokkoli-configuration.nix
@@ -0,0 +1,40 @@
+# This is actually a home-manager config file because the base is WSL Ubuntu
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./home-config/common.nix
+
+    ./profiles/dev.nix
+    ./profiles/writing.nix
+    ./profiles/vrvis.nix
+
+    ./config/nonixos.nix
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "torsney-weir";
+  home.homeDirectory = "/home/torsney-weir";
+
+  home.packages = with pkgs; [
+    # for reviewing papers
+    (callPackage ./pkgs/summ_paper {})
+
+    meld
+    youtube-dl
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+}
