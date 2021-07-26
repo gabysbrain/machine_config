b751f057c04490955e33475475036ba0838c9c33
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-07-26 10:27:10 +0200

    update power management stuff for laptops

--- nixos/laptop.nix
+++ nixos/laptop.nix
@@ -11,6 +11,18 @@
 
   boot.extraModprobeConfig = "options snd_hda_intel power_save=1";
 
+  powerManagement.powertop.enable = true;
+  services.thermald.enable = true;
+
+  # change cpu performance if on battery
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+ };
+
   environment.systemPackages = with pkgs; [
     (callPackage ../pkgs/screenselect.nix {})
     (callPackage ../pkgs/speakerselect.nix {})
