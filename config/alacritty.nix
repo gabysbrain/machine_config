60610783df721520a7a7b30ae3b1602699f52b8d
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-09-09 17:13:57 +0200

    switch alacritty to nord colorscheme

--- config/alacritty.nix
+++ config/alacritty.nix
@@ -23,36 +23,77 @@
         italic.family = "Anonymice Nerd Font";
 
       };
-      # Tender color scheme
+      # Nord color scheme
+      # see https://github.com/arcticicestudio/nord-alacritty/blob/develop/src/nord.yml
       colors = {
         # Default colors
         primary = {
-          background = "0x282828";
-          foreground = "0xeeeeee";
+          background = "0x2e3440";
+          foreground = "0xd8dee9";
+          dim_foreground = "0xa5abb6";
+        };
+
+        cursor = {
+          text = "#2e3440";
+          cursor = "#d8dee9";
+        };
+
+        vi_mode_cursor = {
+          text = "#2e3440";
+          cursor = "#d8dee9";
+        };
+
+        selection = {
+          text = "CellForeground";
+          background = "#4c566a";
+        };
+
+        search = {
+          matches = {
+            foreground = "CellBackground";
+            background = "#88c0d0";
+          };
+
+          bar = {
+            background = "#434c5e";
+            foreground = "#d8dee9";
+          };
         };
 
         # Normal colors
         normal = {
-          black =   "0x282828";
-          red =     "0xf43753";
-          green =   "0xc9d05c";
-          yellow =  "0xffc24b";
-          blue =    "0xb3deef";
-          magenta = "0xd3b987";
-          cyan =    "0x73cef4";
-          white =   "0xeeeeee";
+          black =   "0x3b4252";
+          red =     "0xbf616a";
+          green =   "0xa3be8c";
+          yellow =  "0xebcb8b";
+          blue =    "0x81a1c1";
+          magenta = "0xb48ead";
+          cyan =    "0x88c0d0";
+          white =   "0xe5e9f0";
+        };
+
+        # dim colors
+        dim = {
+          black = "#373e4d";
+          red = "#94545d";
+          green = "#809575";
+          yellow = "#b29e75";
+          blue = "#68809a";
+          magenta = "#8c738c";
+          cyan = "#6d96a5";
+          white = "#aeb3bb";
         };
 
         # Bright colors
         bright = {
-          black =   "0x4c4c4c";
-          red =     "0xf43753";
-          green =   "0xc9d05c";
-          yellow =  "0xffc24b";
-          blue =    "0xb3deef";
-          magenta = "0xd3b987";
-          cyan =    "0x73cef4";
-          white =   "0xfeffff";
+          black =   "0x4c566a";
+          red =     "0xbf616a";
+          green =   "0xa3be8c";
+          yellow =  "0xebcb8b";
+          blue =    "0x81a1c1";
+          magenta = "0xb48ead";
+          cyan =    "0x8fbcbb";
+          white =   "0xeceff4";
         };
       };
     };
