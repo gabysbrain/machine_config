75af936f57cd4451e22c169f3a19dfdd149aad6e
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-09-10 09:59:29 +0200

    less window padding

--- config/alacritty.nix
+++ config/alacritty.nix
@@ -8,8 +8,8 @@
         "WINIT_HIDPI_FACTOR" = "1"; # fix alacritty font scaling issue
       };
       window = {
-        padding.x = 10;
-        padding.y = 10;
+        padding.x = 3;
+        padding.y = 3;
         #decorations = "buttonless";
       };
       scrolling.history = 10000;
