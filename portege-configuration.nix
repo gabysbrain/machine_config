fe4e8a9064769535735ef194a6a0dc8e8a70fceb
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-11-10 11:11:48 +0100

    fix buggy intel wireless driver

--- portege-configuration.nix
+++ portege-configuration.nix
@@ -27,6 +27,14 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.extraModprobeConfig = ''
+    "options iwlwifi 11n_disable=1"
+    "options iwlwifi swcrypto=0"
+    "options iwlwifi bt_coex_active=0"
+    "options iwlwifi power_save=0"
+    "options iwlwifi uapsd_disable=1"
+    "options iwlmvm power_scheme=1"
+  '';
 
   # for building nixos on other systems (e.g. raspberry pi)
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
