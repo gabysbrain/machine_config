45dedda041bffcacdfaad612caa2f837043526f0
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-12-11 20:23:46 +0100

    network status script is more flexible now

--- config/xmonad/net.sh
+++ config/xmonad/net.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 net_up () {
-  ip link show | grep $1 | grep 'state UP' > /dev/null
+  ip link show | grep "$1" | grep 'state UP' > /dev/null
 }
 
-if net_up wlp2s0; then
+if net_up wlp; then
   WLAN=直
 else
   WLAN="<fc=#dddddd>睊</fc>"
 fi
 
-if net_up eth0; then
+if (net_up enp) || (net_up eth); then
   ETH=
 else
   ETH="<fc=#dddddd></fc>"
