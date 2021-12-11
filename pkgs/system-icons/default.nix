a74292399cd918a1d15eae36721517360e34bd81
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-12-11 20:01:16 +0100

    add katana icon for login

--- pkgs/system-icons/default.nix
+++ pkgs/system-icons/default.nix
@@ -2,7 +2,7 @@
 #with import <nixpkgs> {};
 
 # TODO: pick up the list of icons automatically
-let systems = [ "philadelphia" "brokkoli" ];
+let systems = [ "philadelphia" "brokkoli" "katana" ];
 in stdenv.mkDerivation {
   name = "system-icons";
   description = "icons for lightdm for each GUI system";
@@ -17,6 +17,7 @@ in stdenv.mkDerivation {
 
     ln -s $src/philadelphia.png $out/share/icons/64x64/
     ln -s $src/brokkoli.png $out/share/icons/64x64/
+    ln -s $src/katana.png $out/share/icons/64x64/
   '';
 } 
 
