3a5f0f4436914a2de01fcdc935a65046489b08a1
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-12-11 20:03:02 +0100

    bump julia plugin version

--- config/vscode/default.nix
+++ config/vscode/default.nix
@@ -19,11 +19,11 @@
       publisher = "mrcrowl";
       version = "1.7.1";
       sha256 = "1qlnc77qhr992yk5zzxvagh157lfg47ax6k1zl9smd07c1jg7l80";
-    } {
+    } /*{
       name = "language-julia";
       publisher = "julialang";
-      version = "1.3.33";
-      sha256 = "11nag14jycj1dzxa4w97c0y160hlwaxghrjvcm1aq1q5k5k1gd4d";
-    }];
+      version = "1.5.6";
+      sha256 = "0dhiqsmj7wi5bqfl1sd5wdkqy48a08hkd133svp2ldcr5jhw4kks";
+    }*/];
   };
 }
