e8fa3cfd621f63ddd33be4e5a4da3c5cd870b25b
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-12-12 12:30:39 +0100

    connect/disconnect shouldn't need sshfs installed

--- pkgs/vrvis/default.nix
+++ pkgs/vrvis/default.nix
@@ -13,5 +13,9 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     cp $src/vrvis_connect $out/bin/
     cp $src/vrvis_disconnect $out/bin/
+
+    # fix sshfs path
+    sed -i 's#^sshfs#${pkgs.sshfs}/bin/sshfs#' $out/bin/vrvis_connect
+    sed -i 's#^sshfs#${pkgs.sshfs}/bin/sshfs#' $out/bin/vrvis_disconnect
   '';
 }
