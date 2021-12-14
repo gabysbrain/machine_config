3eea0144410b2d3ed84c07e0e4fb156a5f81db66
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-12-14 19:52:56 +0100

    ssh port forwarding alias

--- config/zsh.nix
+++ config/zsh.nix
@@ -113,6 +113,23 @@
         history 0 | grep -i $*
       }
 
+      # function for easy port forwarding
+      function fwdport {
+        readonly conn=''${1:?"The host must be specified"}
+        readonly myport=''${2:?"The port must be specified"}
+
+        # if only one port is given then just use it for both
+        if [ -z "$3" ]; then
+          readonly dstport=$myport
+        else
+          readonly dstport=$3
+        fi
+
+        #ssh -N -L ''${srcport}:''${host}:''${dstport} ''${conn}
+        ssh -N -L ''${myport}:localhost:''${dstport} ''${conn}
+      }
+
+
       if [ -f ~/.config/zsh/scratch.zsh ]; then
         source ~/.config/zsh/scratch.zsh
       fi
