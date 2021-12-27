1dcfb7e21f6ec30aeb69190ba47548eb9c9645e3
Thomas Torsney-Weir <torsneyt@gmail.com>
2021-12-27 11:01:07 +0100

    also backup grocy

--- ops/grocy.nix
+++ ops/grocy.nix
@@ -15,5 +15,28 @@
       calendar.firstDayOfWeek = 1; # monday
     };
   };
+
+  # backup grocy stuff
+  age.secrets.wasabi.file = ../secrets/wasabi.age;
+  age.secrets.restic.file = ../secrets/restic.age;
+  services.restic.backups.remote = {
+    paths = [ "/var/lib/grocy" ];
+    repository = "s3:https://s3.eu-central-1.wasabisys.com/gabysbrain-restic";
+    passwordFile = "/run/agenix/restic"; # FIXME: this should use age.secrets.restic.path somehow
+    environmentFile = "/run/agenix/wasabi"; # FIXME: this should use age.secrets.wasbi.path
+    extraBackupArgs = [
+      "--no-cache"
+    ];
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 8"
+      "--keep-monthly 24"
+      "--keep-yearly 5"
+      "--keep-last 2"
+    ];
+    timerConfig = {
+      OnCalendar = "02:15";
+    };
+  };
 }
 
