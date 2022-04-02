6ea0fb314616f46011ff06424010708c69e89bbb
Thomas Torsney-Weir <torsneyt@gmail.com>
2022-04-02 12:37:31 +0200

    trying to override xdg commands

--- config/mimeo/default.nix
+++ config/mimeo/default.nix
@@ -1,5 +1,28 @@
 { pkgs, ... }:
 
+let 
+  xdg-open = pkgs.writeShellApplication {
+    name = "xdg-open";
+    runtimeInputs = with pkgs; [ mimeo ];
+    text = ''
+      mimeo "$1"
+    '';
+  };
+  xdg-email = pkgs.writeShellApplication {
+    name = "xdg-email";
+    runtimeInputs = with pkgs; [ mimeo ];
+    text = ''
+      mimeo "$1"
+    '';
+  };
+  xdg-mime = pkgs.writeShellApplication {
+    name = "xdg-mime";
+    runtimeInputs = with pkgs; [ mimeo ];
+    text = ''
+      mimeo -m "$1"
+    '';
+  };
+in
 {
   home.file = {
     ".config/mimeo/associations.txt".source = ./dot-associations;
@@ -7,5 +30,9 @@
 
   home.packages = with pkgs; [
     mimeo
+
+    # patched xdg-utils
+    xdg-open
+    xdg-mime
   ];
 }
