9bd0fb9a1bdcbb3e65ce45676bf4a6af45d20424
Thomas Torsney-Weir <torsneyt@gmail.com>
2022-04-03 12:54:08 +0200

    switch to pipewire with low-latency config

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -60,12 +60,59 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # low-latency config
+    config.pipewire-pulse = {
+      "context.properties" = {
+        "log.level" = 2;
+      };
+      "context.modules" = [
+        {
+          name = "libpipewire-module-rtkit";
+          args = {
+            "nice.level" = -15;
+            "rt.prio" = 88;
+            "rt.time.soft" = 200000;
+            "rt.time.hard" = 200000;
+          };
+          flags = [ "ifexists" "nofail" ];
+        }
+        { name = "libpipewire-module-protocol-native"; }
+        { name = "libpipewire-module-client-node"; }
+        { name = "libpipewire-module-adapter"; }
+        { name = "libpipewire-module-metadata"; }
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            "pulse.min.req" = "32/48000";
+            "pulse.default.req" = "32/48000";
+            "pulse.max.req" = "32/48000";
+            "pulse.min.quantum" = "32/48000";
+            "pulse.max.quantum" = "32/48000";
+            "server.address" = [ "unix:native" ];
+          };
+        }
+      ];
+      "stream.properties" = {
+        "node.latency" = "32/48000";
+        "resample.quality" = 1;
+      };
+    };
+    media-session.config.alsa-monitor = {
+      rules = [
+        {
+          matches = [ { "node.name" = "alsa_output.*"; } ];
+          actions = {
+            update-props = {
+              "audio.format" = "S32LE";
+              "audio.rate" = 96000; # for USB soundcards it should be twice your desired rate
+              "api.alsa.period-size" = 32; # defaults to 1024, tweak by trial-and-error
+              #"api.alsa.disable-batch" = true; # generally, USB soundcards use the batch mode
+            };
+          };
+        }
+      ];
+    };
   };
 
   # Enable the X11 windowing system.
