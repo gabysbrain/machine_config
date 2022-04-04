bfe00678aa902a2d36399a8116bbaf94ae04a886
Thomas Torsney-Weir <torsneyt@gmail.com>
2022-04-04 11:56:58 +0200

    typo

--- nixos/common.nix
+++ nixos/common.nix
@@ -4,7 +4,7 @@
   nix = {
     package = pkgs.nixUnstable; # or versioned attributes like nix_2_4
     extraOptions = ''
-      experimental features = nix-command flakes
+      experimental-features = nix-command flakes
     '';
   };
 
