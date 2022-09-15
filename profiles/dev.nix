a26ec82dc9fc7a4f7af252ff7e8ec024563524ee
Thomas Torsney-Weir <torsneyt@gmail.com>
2022-09-15 10:27:00 +0200

    bump version and needed to update julia

--- profiles/dev.nix
+++ profiles/dev.nix
@@ -28,7 +28,7 @@ let #my-hdf5r = pkgs.rPackages.hdf5r.override {
       usethis
       xml2
     ];
-    juliaPkg = pkgs.unstable.julia_17-bin;
+    juliaPkg = pkgs.unstable.julia_18-bin;
 in
 {
   imports = [
