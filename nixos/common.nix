49ea18eb1a1b9da0aa9b49d3bd7d0675edf3b617
Thomas Torsney-Weir <torsneyt@gmail.com>
2022-10-03 12:54:26 +0200

    flake is in stable nix now. don't need unstable nix anymore

--- nixos/common.nix
+++ nixos/common.nix
@@ -1,16 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  nix = {
-    package = pkgs.nixUnstable; # or versioned attributes like nix_2_4
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
   imports = [
-    #<home-manager/nixos>
-    #<agenix/modules/age.nix>
     ../secrets/home-manager.nix
   ];
 
