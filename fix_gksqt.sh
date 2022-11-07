c4bc04398177cec96863165f51dab3f9ceef9ec5
Thomas Torsney-Weir <torsneyt@gmail.com>
2022-11-07 12:03:23 +0100

    update GKSQTID with new version

--- fix_gksqt.sh
+++ fix_gksqt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-GKSQTID=534ab5a2b484a390ae357f2d17ebb0473e160a9b
+#GKSQTID=534ab5a2b484a390ae357f2d17ebb0473e160a9b
+GKSQTID=26099abd23e11fc440259049997307380be745c8
 
 chmod +w ~/.julia/artifacts/${GKSQTID}/bin/gksqt; nix-shell -p patchelf stdenv --command "patchelf --set-interpreter \"\$(cat \${NIX_CC}/nix-support/dynamic-linker)\" ~/.julia/artifacts/${GKSQTID}/bin/gksqt"
 
