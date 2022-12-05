de76ccfbb4286fe2455cd43d8d13e084cce0ff1d
Thomas Torsney-Weir <torsneyt@gmail.com>
2022-12-05 13:53:35 +0100

    better julia lsp initialization

--- config/neovim/lsp.lua
+++ config/neovim/lsp.lua
@@ -57,13 +57,28 @@ local julia_startup = [[
   using LanguageServer, LanguageServer.SymbolServer
   import Pkg
 
+  depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
   # figure out if there's a project to connect to
-  projfile = Base.current_project();
-  if projfile == nothing
-    projfile = Pkg.Types.Context().env.project_file;
+  # from https://github.com/neovim/nvim-lspconfig/blob/2dd9e060f21eecd403736bef07ec83b73341d955/lua/lspconfig/server_configurations/julials.lua#L19-L35
+  project_path = let
+    dirname(something(
+      ## 1. Finds an explicitly set project (JULIA_PROJECT)
+      Base.load_path_expand((
+        p = get(ENV, "JULIA_PROJECT", nothing);
+        p === nothing ? nothing : isempty(p) ? nothing : p
+      )),
+      ## 2. Look for a Project.toml file in the current working directory,
+      ##    or parent directories, with $HOME as an upper boundary
+      Base.current_project(),
+      ## 3. First entry in the load path
+      get(Base.load_path(), 1, nothing),
+      ## 4. Fallback to default global environment,
+      ##    this is more or less unreachable
+      Base.load_path_expand("@v#.#"),
+    ))
   end
-  env_path = dirname(projfile);
-  server = LanguageServer.LanguageServerInstance(stdin, stdout, env_path, "");
+  @info "Running language server" VERSION pwd() project_path depot_path
+  server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path);
   server.runlinter = true;
   run(server);
 ]]
