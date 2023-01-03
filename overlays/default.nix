89d2d68d8c4885a8ccafe0147685b5120c7a3698
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-01-03 12:49:17 +0100

    put slock back

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,16 @@
 final: prev: 
 
 rec {
+  slock = prev.slock.overrideAttrs ( old: {
+    src = prev.fetchFromGitHub {
+      owner = "gabysbrain";
+      repo = "slock";
+      rev = "37f091cb167f719103ef70baa6b46b95645e5b95";
+      #sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "0q2l3y6pm817y0gib3z0bq0h3c95lcys0wz9vmlb779q76spkr7v";
+    };
+  });
+
   #openrgb = (import ../overlays/openrgb.nix final prev);
 
   python = prev.python.override {
