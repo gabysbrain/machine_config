a223c21efa9622f42ab1d1e7f79f3336a8a6845e
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-01-26 12:30:28 +0100

    switch to shutter for screenshots

--- pkgs/screenshot/default.nix
+++ pkgs/screenshot/default.nix
@@ -1,11 +1,10 @@
 {pkgs ? import <nixpkgs>}:
 #with import <nixpkgs> {};
 
-pkgs.runCommand "screenshot" {
-  buildInputs = with pkgs; [ maim dmenu xdotool ];
-} ''
-  mkdir -p $out/bin
-  cp ${./screenshot} $out/bin/screenshot
-  sed -i "2 i export PATH=$PATH" $out/bin/screenshot
-''
+pkgs.writeShellApplication {
+  name = "screenshot";
 
+  runtimeInputs = with pkgs; [ shutter dmenu ];
+
+  text = builtins.readFile ./screenshot;
+}
