158ee8b7b90c3bd1ae510fbf6254f9ce7a1ad6bc
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-03-08 15:40:43 +0100

    formatting

--- raicoon-configuration.nix
+++ raicoon-configuration.nix
@@ -51,18 +51,19 @@
 
   boot.loader.grub = {
     enable = true;
-version = 2;
-efiSupport = true;
-enableCryptodisk = true;
-device = "nodev";
+    version = 2;
+    efiSupport = true;
+    enableCryptodisk = true;
+    device = "nodev";
   };
 
+  # encrypted disk setup
   boot.initrd.luks.devices = {
-crypted = {
-device = "/dev/nvme0n1p2";
-preLVM = true;
-};
-};
+    crypted = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+  };
 
   networking.hostName = "raicoon"; # Define your hostname.
   # Pick only one of the below networking options.
