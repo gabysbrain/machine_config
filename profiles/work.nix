c1d3a4d40a0ed1f42d9d7c0ff961b5286fd58a84
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-03-15 15:47:38 +0100

    use work email

--- profiles/work.nix
+++ profiles/work.nix
@@ -1,6 +1,21 @@
 { pkgs, ... }:
 
 {
+  programs = {
+    git = {
+      includes = [
+        { contents = {
+            user = {
+              name = "Tom Torsney-Weir";
+              email = "t.torsney@raicoon.com";
+            };
+          };
+          condition = "gitdir:~/raicoon/";
+        }
+      ];
+    };
+  };
+
   home.packages = with pkgs; [
     slack
   ];
