bac08518ccfd7093fe3b1543e73b4c0105be3443
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-05-04 22:03:31 +0200

    nix-ld makes this obsolete

--- fix_gksqt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#GKSQTID=534ab5a2b484a390ae357f2d17ebb0473e160a9b
-#GKSQTID=26099abd23e11fc440259049997307380be745c8
-GKSQTID=ba11969bdd27bbb55be64bfadc0de51a013e945a
-
-chmod +w ~/.julia/artifacts/${GKSQTID}/bin/gksqt; nix-shell -p patchelf stdenv --command "patchelf --set-interpreter \"\$(cat \${NIX_CC}/nix-support/dynamic-linker)\" ~/.julia/artifacts/${GKSQTID}/bin/gksqt"
-
