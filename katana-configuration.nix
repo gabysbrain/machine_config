7174ab3d77546e37b218764cf5f99e7bc7147ac6
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-06-04 10:35:59 +0200

    feature: katana needs to print

--- katana-configuration.nix
+++ katana-configuration.nix
@@ -146,6 +146,13 @@
     };
   };
 
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  #services.avahi.enable = true;
+  #services.avahi.nssmdns = true;
+  #services.avahi.openFirewall = true;
+  services.printing.drivers = [ pkgs.brlaser ];
+
   # virtualization
   virtualisation.libvirtd.enable = true;
   users.extraGroups.libvirtd.members = [ "tom" ];
