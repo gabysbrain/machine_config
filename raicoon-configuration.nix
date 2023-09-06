7f75e45631fafb3b03cc90c8549dfd869cfe0ecf
Thomas Torsney-Weir <torsneyt@gmail.com>
2023-09-06 13:34:48 +0200

    netowrking: don't need wireless set at top level

--- raicoon-configuration.nix
+++ raicoon-configuration.nix
@@ -66,9 +66,6 @@
   };
 
   networking.hostName = "raicoon"; # Define your hostname.
-  # Pick only one of the below networking options.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  #networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Europe/Vienna";
