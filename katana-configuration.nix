1f9998d5cdbcfb4fb3a74f02c2c2063077245c33
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-04-02 09:41:05 +0200

    katana: dm-mirror kernel mod is needed for moving lvm stuff around

--- katana-configuration.nix
+++ katana-configuration.nix
@@ -21,7 +21,7 @@
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" "dm-mirror" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
   boot.extraModprobeConfig = ''
