956d94f1f2b6346a0429352d52c25d541313f26e
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-11-25 12:33:45 +0000

    new system: wsl work computer

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,6 +19,7 @@
     self, 
     nixpkgs, 
     nixpkgs-unstable, 
+    nixos-wsl,
     home-manager, 
     agenix, 
     homeage 
@@ -34,6 +37,31 @@
 
     # FIXME: tom user should be created and have zsh shell
     nixosConfigurations = {
+      work = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ 
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ my-overrides extra-pkgs-overlay ]; })
+          nixos-wsl.nixosModules.default
+          ./work-configuration.nix 
+          #agenix.nixosModules.default
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.tom = { pkgs, nixosConfig, ... }: {
+              home.stateVersion = "24.05";
+
+              imports = [
+                ./home-config/common.nix
+                ./home-config/desktop.nix
+                ./profiles/dev.nix
+
+                # FIXME: not sure why this breaks in home-config/common...
+                homeage.homeManagerModules.homeage
+              ];
+            };
+          }
+        ];
+      };
       philadelphia = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ 
