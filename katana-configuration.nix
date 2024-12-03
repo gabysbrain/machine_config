8efd928df40f58b2eb6b6e1ec9ed528e6a57867e
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-12-03 12:54:57 +0100

    refactor: hardware.opengl is now hardware.graphics

--- katana-configuration.nix
+++ katana-configuration.nix
@@ -6,7 +6,7 @@
 
 {
   imports =
-    [ ./nixos/nvidia-fix.nix
+    [ #./nixos/nvidia-fix.nix
       ./nixos/common.nix
       ./nixos/games.nix
       ./nixos/desktop.nix
@@ -63,9 +63,35 @@
 
   # Video drivers setup
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
+  };
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = true;
+
+    # Enable the Nvidia settings menu,
+	  # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
   };
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
