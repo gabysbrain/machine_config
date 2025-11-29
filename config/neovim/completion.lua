8aa1c8d2d844f88d5c6e3729a15de6a6875d607e
Thomas Torsney-Weir <torsneyt@gmail.com>
2025-11-29 15:04:50 +0100

    fix: tab gets confusing with completion vs actual tabs so use default for suggestions

--- config/neovim/completion.lua
+++ config/neovim/completion.lua
@@ -13,7 +13,7 @@ cmp.setup({
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'super-tab' },
+    keymap = { preset = 'default' },
 
     appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
