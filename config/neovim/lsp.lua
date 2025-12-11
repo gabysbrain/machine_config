f522ca16c5b984852e11dcf354e0f381b79aec24
Tom Torsney-Weir <thomas.torsney-weir@boehringer-ingelheim.com>
2025-12-11 11:17:20 +0000

    feat: the lspconfig framework is replaced with vim.lsp.config

--- config/neovim/lsp.lua
+++ config/neovim/lsp.lua
@@ -1,48 +1,49 @@
-local nvim_lsp = require('lspconfig')
-local util = nvim_lsp.util
-
 --local capabilities = vim.lsp.protocol.make_client_capabilities()
 local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
 
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    --buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
+    -- Mappings.
+    local opts = { noremap = true, silent = true }
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-h>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('i', '<C-h>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- error handling
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '<leader>Q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.keymap.set('n', '<C-h>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.keymap.set('i', '<C-h>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.keymap.set('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    -- error handling
+    vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    vim.keymap.set('n', '<leader>Q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 
-  -- TODO: see if I ever want these
-  --buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  --buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  --buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  --buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  --buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  --buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  --buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-end
+    -- TODO: see if I ever want these
+    --vim.keymap.set('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    --vim.keymap.set('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    --vim.keymap.set('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    --vim.keymap.set('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    --vim.keymap.set('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    --vim.keymap.set('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    --vim.keymap.set('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  end,
+})
 
--- setup each language server here
+vim.lsp.enable({ 'julials', 'ts_ls', 'eslint', 'gopls', 'hls', 'lua_ls', 'nil_ls', 'pyright', 'rust_analyzer', 'texlab', })
+
+-- specific language configs
 -- julia
 local julia_startup = [[
   using LanguageServer, LanguageServer.SymbolServer
@@ -73,49 +74,15 @@ local julia_startup = [[
   server.runlinter = true;
   run(server);
 ]]
-nvim_lsp['julials'].setup {
+
+vim.lsp.config('julials', {
   cmd = { "julia", "--startup-file=no", "--history-file=no", "-e", julia_startup },
-  on_attach = on_attach,
-  capabilities = capabilities,
   flags = {
     debounce_text_changes = 150,
   }
-}
+})
 
-nvim_lsp['ts_ls'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-nvim_lsp['eslint'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-nvim_lsp['gopls'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-nvim_lsp['hls'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-nvim_lsp['lua_ls'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-nvim_lsp['nil_ls'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-nvim_lsp['pyright'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-
+vim.lsp.config('pyright', {
   settings = {
     pyright = {
       -- I run isort on save
@@ -144,27 +111,16 @@ nvim_lsp['pyright'].setup {
       }
     }
   }
-}
+})
 
-nvim_lsp['ruff'].setup {
+vim.lsp.config('ruff', {
   on_attach = function(client, bufnr)
     -- disable hover in favor of pyright
     client.server_capabilities.hoverProvider = false
   end,
-  capabilities = capabilities,
   init_options = {
     settings = {
       args = { "--extend-select", "T", },
     },
   },
-}
-
-nvim_lsp['rust_analyzer'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
-
-nvim_lsp['texlab'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+})
