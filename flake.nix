1d24acb49b7315a36bacf8fc02d8dd187b8f4393
Tom Torsney-Weir <thomas.torsney-weir@boehringer-ingelheim.com>
2026-02-16 11:49:13 +0000

    fix: not using internal cachix for now

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "My system config flake";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://cachix.joukamachi.net/prod"
-    ];
-    extra-trusted-public-keys = [
-      "prod:YvdQaSxvCua1bSMOD3JQj7eexVTZhmeHWWY842+T+aM="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -24,109 +15,138 @@
     homeage.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { 
-    self, 
-    nixpkgs, 
-    nixpkgs-unstable, 
-    nixos-wsl,
-    home-manager, 
-    agenix, 
-    homeage 
-  }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      nixos-wsl,
+      home-manager,
+      agenix,
+      homeage,
+    }@inputs:
     let
       extra-pkgs-overlay = final: prev: {
         unstable = import nixpkgs-unstable {
-          system=prev.system;
+          system = prev.system;
           config.allowUnfree = true; # FIXME: doesn't work with nixos-install
         };
         inherit agenix;
       };
       my-overrides = import overlays/default.nix;
-      overlays = [ extra-pkgs-overlay my-overrides ];
-    in {
+      overlays = [
+        extra-pkgs-overlay
+        my-overrides
+      ];
+    in
+    {
 
-    # FIXME: tom user should be created and have zsh shell
-    nixosConfigurations = {
-      work = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ({ config, pkgs, ... }: { nixpkgs.overlays = overlays; })
-          nixos-wsl.nixosModules.default
-          ./work-configuration.nix 
-          #agenix.nixosModules.default
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.torsneyw = { pkgs, nixosConfig, ... }: {
-              home.stateVersion = "24.11";
+      # FIXME: tom user should be created and have zsh shell
+      nixosConfigurations = {
+        work = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (
+              { config, pkgs, ... }:
+              {
+                nixpkgs.overlays = overlays;
+              }
+            )
+            nixos-wsl.nixosModules.default
+            ./work-configuration.nix
+            #agenix.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.torsneyw =
+                { pkgs, nixosConfig, ... }:
+                {
+                  home.stateVersion = "24.11";
 
-              imports = [
-                ./home-config/common.nix
-                ./home-config/desktop.nix
-                ./profiles/dev.nix
-                ./profiles/work.nix
+                  imports = [
+                    ./home-config/common.nix
+                    ./home-config/desktop.nix
+                    ./profiles/dev.nix
+                    ./profiles/work.nix
 
-                # FIXME: not sure why this breaks in home-config/common...
-                homeage.homeManagerModules.homeage
-              ];
-            };
-          }
-        ];
+                    # FIXME: not sure why this breaks in home-config/common...
+                    homeage.homeManagerModules.homeage
+                  ];
+                };
+            }
+          ];
+        };
+        philadelphia = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (
+              { config, pkgs, ... }:
+              {
+                nixpkgs.overlays = overlays;
+              }
+            )
+            ./portege-configuration.nix
+            agenix.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.tom =
+                { pkgs, nixosConfig, ... }:
+                {
+                  home.stateVersion = "20.09";
+
+                  imports = [
+                    ./home-config/common.nix
+                    ./home-config/desktop.nix
+                    ./profiles/dev.nix
+                    ./profiles/writing.nix
+
+                    # FIXME: not sure why this breaks in home-config/common...
+                    homeage.homeManagerModules.homeage
+                  ];
+                };
+            }
+          ];
+        };
+        katana = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (
+              { config, pkgs, ... }:
+              {
+                nixpkgs.overlays = overlays;
+              }
+            )
+            ./katana-configuration.nix
+            agenix.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.tom =
+                { pkgs, nixosConfig, ... }:
+                {
+                  home.stateVersion = "20.09";
+                  imports = [
+                    ./home-config/common.nix
+                    ./home-config/desktop.nix
+                    ./profiles/dev.nix
+                    ./profiles/games.nix
+                    ./profiles/writing.nix
+
+                    # FIXME: not sure why this breaks in home-config/common...
+                    homeage.homeManagerModules.homeage
+                  ];
+
+                  # adjust terminal for high dpi screen
+                  programs.kitty.font.size = pkgs.lib.mkForce 14;
+                };
+            }
+          ];
+        };
       };
-      philadelphia = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ({ config, pkgs, ... }: { nixpkgs.overlays = overlays; })
-          ./portege-configuration.nix 
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.tom = { pkgs, nixosConfig, ... }: {
-              home.stateVersion = "20.09";
 
-              imports = [
-                ./home-config/common.nix
-                ./home-config/desktop.nix
-                ./profiles/dev.nix
-                ./profiles/writing.nix
-
-                # FIXME: not sure why this breaks in home-config/common...
-                homeage.homeManagerModules.homeage
-              ];
-            };
-          }
-        ];
-      };
-      katana = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ({ config, pkgs, ... }: { nixpkgs.overlays = overlays; })
-          ./katana-configuration.nix 
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.tom = { pkgs, nixosConfig, ... }: {
-              home.stateVersion = "20.09";
-              imports = [
-                ./home-config/common.nix
-                ./home-config/desktop.nix
-                ./profiles/dev.nix
-                ./profiles/games.nix
-                ./profiles/writing.nix
-
-                # FIXME: not sure why this breaks in home-config/common...
-                homeage.homeManagerModules.homeage
-              ];
-
-              # adjust terminal for high dpi screen
-              programs.kitty.font.size = pkgs.lib.mkForce 14;
-            };
-          }
-        ];
-      };
     };
-
-  };
 }
